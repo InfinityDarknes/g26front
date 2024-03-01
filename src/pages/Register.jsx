7286ce709ad70833543b1028ea8ab6584b8d50db
InfinityDarknes <brayan-diaz1996@outlook.com>
2024-02-29 21:40:07 -0600

    Se creo toda la insercion de datos de registrar usuario

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {toast} from "react-toastify"
 import { FaUser } from "react-icons/fa";
+import {reset, register} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,12 +16,47 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state)=> state.auth)
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+
+  const onSubmit =(e) =>{
+    e.preventDefault()
+
+    if(password !== password2){
+      toast.error('La contraseña no coincide intente de nuevo')
+    }
+    else{
+      const userData = {
+         name,
+         email,
+         password
+        }
+        dispatch(register(userData))
+    }
+  }
+  useEffect(()=>{
+    if(isError){      
+      toast.error(message)
+    }
+    if(isSuccess){
+      navigate('/login')
+    }
+    dispatch(reset())
+
+  },[user, isError,isSuccess,message,navigate,dispatch])
+
+  if(isLoading){
+    return <Spinner/>
+  }
 
   return (
     <>
@@ -29,7 +69,7 @@ const Register = () => {
       </section>
 
       <section className="form">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="text"
@@ -40,6 +80,44 @@ const Register = () => {
               placeholder="Por favor escribe tu nombre"
               onChange={onChange}
             />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Por favor escribe tu email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Por favor escribe tu Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Por favor confirma tu Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type = "submit" className="btn btn-block">
+              Crear
+            </button>
           </div>
         </form>
       </section>
