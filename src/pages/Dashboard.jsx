0e903386b6b4f3d0cfc8cd8881a58ae7021ae67e
InfinityDarknes <brayan-diaz1996@outlook.com>
2024-03-12 20:49:25 -0600

    Arreglamos el ciclado

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,7 +21,9 @@ const Dashboard = () => {
     if(!user){
       navigate('/login')
     }
-    dispatch(getTareas())
+    else{
+      dispatch(getTareas())
+    }
 
     return ()=>{
       dispatch(reset())
